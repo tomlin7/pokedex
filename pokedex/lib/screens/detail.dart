866455648e8af0463a01bab430909d8cc8df29e6
Billy <billydevbusiness@gmail.com>
2024-12-31 18:26:39 +0530

    feat: Pokemon details page design

---
+++ pokedex/lib/screens/detail.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/models/pokemon.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Pokemon pokemon;
+
+  const DetailScreen({super.key, required this.pokemon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                  const SizedBox(width: 20),
+                  Text(
+                    pokemon.name,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(),
+                  Text(
+                    '#${pokemon.number.toString().padLeft(3, '0')}',
+                    style: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: pokemon.backgroundColor,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      pokemon.imageUrl,
+                      height: 200,
+                    ),
+                    const SizedBox(height: 20),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("Forms"),
+                        Text('Detail'),
+                        Text('Types'),
+                        Text('Stats'),
+                        Text('Weak'),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Mega Evolution',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      pokemon.description,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
